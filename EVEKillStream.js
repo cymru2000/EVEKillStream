f1ce71de34a2df66e6b63bacff75d8050baba952
Wayne <wayne.hornsey@gmail.com>
2020-07-24 15:56:58 +0100

    Update EVEKillStream.js

--- EVEKillStream.js
+++ EVEKillStream.js
@@ -13,7 +13,7 @@ const ws = new WebSocket('wss://zkillboard.com:2096')
 
 //Define the dev HTTP server settings
 const host = 'localhost';
-const port = 443;
+const port = 8000;
 
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
